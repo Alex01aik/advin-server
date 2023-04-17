e331ffddcb06ffb651c51f794779187addfff970
a.petrov <a.petrov@kuma.family>
2023-04-17 16:32:07 +0200

    remove app controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { SchemaModule } from './schema/schema.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { CompanyModule } from './company/company.module';
 import { S3Module } from './s3/s3.module';
 
 @Module({
-  imports: [AuthModule, UserModule, CompanyModule, S3Module, SchemaModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, UserModule, CompanyModule, S3Module],
 })
 export class AppModule {}
